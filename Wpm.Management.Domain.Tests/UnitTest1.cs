94974658b46c45c1241bf843fd09e307ad85b8a6
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-08-20 10:42:01 -0700

    01_02e

--- Wpm.Management.Domain.Tests/UnitTest1.cs
+++ Wpm.Management.Domain.Tests/UnitTest1.cs
@@ -3,8 +3,35 @@ namespace Wpm.Management.Domain.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Pet_should_be_equal()
     {
+        var id = Guid.NewGuid();
+        var pet1 = new Pet() { Id = id };
+        var pet2 = new Pet() { Id = id };
 
+        Assert.True(pet1.Equals(pet2));
+    }
+    
+    [Fact]
+    public void Pet_should_be_equal_using_operators()
+    {
+        var id = Guid.NewGuid();
+        var pet1 = new Pet() { Id = id };
+        var pet2 = new Pet() { Id = id };
+
+        Assert.True(pet1 == pet2);
+    }
+
+
+    [Fact]
+    public void Pet_should_not_be_equal_using_operators()
+    {
+        var id1 = Guid.NewGuid();
+        var id2 = Guid.NewGuid();
+
+        var pet1 = new Pet() { Id = id1 };
+        var pet2 = new Pet() { Id = id2 };
+
+        Assert.True(pet1 != pet2);
     }
 }
