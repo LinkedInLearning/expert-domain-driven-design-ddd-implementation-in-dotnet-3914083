9c68418040c9babd355636371fe5f33a2c9fa4bb
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-08-20 20:03:02 -0700

    01_06e

--- Wpm.Management.Domain.Tests/UnitTest1.cs
+++ Wpm.Management.Domain.Tests/UnitTest1.cs
@@ -6,8 +6,8 @@ public class UnitTest1
     public void Pet_should_be_equal()
     {
         var id = Guid.NewGuid();
-        var pet1 = new Pet(id) { Name = "Gianni", Age = 13 };
-        var pet2 = new Pet(id) { Name = "Nina", Age = 10 };
+        var pet1 = new Pet(id, "Gianni", 13, "Three-color", new Weight(20.5m), SexOfPet.Male);
+        var pet2 = new Pet(id, "Nina", 10, "Three-color", new Weight(18.5m), SexOfPet.Female);
 
         Assert.True(pet1.Equals(pet2));
     }
@@ -16,8 +16,8 @@ public class UnitTest1
     public void Pet_should_be_equal_using_operators()
     {
         var id = Guid.NewGuid();
-        var pet1 = new Pet(id);
-        var pet2 = new Pet(id);
+        var pet1 = new Pet(id, "Gianni", 13, "Three-color", new Weight(20.5m), SexOfPet.Male);
+        var pet2 = new Pet(id, "Nina", 10, "Three-color", new Weight(18.5m), SexOfPet.Female);
 
         Assert.True(pet1 == pet2);
     }
@@ -29,9 +29,17 @@ public class UnitTest1
         var id1 = Guid.NewGuid();
         var id2 = Guid.NewGuid();
 
-        var pet1 = new Pet(id1);
-        var pet2 = new Pet(id2);
+        var pet1 = new Pet(id1, "Gianni", 13, "Three-color", new Weight(20.5m), SexOfPet.Male);
+        var pet2 = new Pet(id2, "Nina", 10, "Three-color", new Weight(18.5m), SexOfPet.Female);
 
         Assert.True(pet1 != pet2);
+    }
+
+    [Fact]
+    public void Weight_should_be_equal()
+    {
+        var w1 = new Weight(20.5m);
+        var w2 = new Weight(20.5m);
+        Assert.True(w1 == w2);
     }
 }
---
+++ Wpm.Management.Domain/Weight.cs
@@ -0,0 +1,15 @@
+﻿namespace Wpm.Management.Domain;
+public record Weight
+{
+    public decimal Value { get; init; }
+
+    public Weight(decimal value)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentException("Weight value is not valid.");
+        }
+
+        Value = value;
+    }
+}
