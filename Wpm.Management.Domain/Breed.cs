1251017f579412ec19b2e3021213e17169d6592c
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-08-20 20:45:57 -0700

    01_07e

--- Wpm.Management.Domain/Breed.cs
+++ Wpm.Management.Domain/Breed.cs
@@ -1,10 +1,9 @@
 ﻿namespace Wpm.Management.Domain;
 public class Breed : Entity
 {
-    public string Name { get; set; }
-
-    public WeightRange MaleIdealWeight { get; set; }
-    public WeightRange FemaleIdealWeight { get; set; }
+    public string Name { get; init; }
+    public WeightRange MaleIdealWeight { get; init; }
+    public WeightRange FemaleIdealWeight { get; init; }
 
     public Breed(string name,
                  WeightRange maleIdealWeight,
