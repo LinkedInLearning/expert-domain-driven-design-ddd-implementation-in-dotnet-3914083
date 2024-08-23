aaab63318158e5c86975a4db044c6407c936f1e8
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2024-08-23 11:27:21 -0700

    05_03e

--- Wpm.Management.Api/Application/SetWeightCommandHandler.cs
+++ Wpm.Management.Api/Application/SetWeightCommandHandler.cs
@@ -1,11 +1,25 @@
 ﻿using Wpm.Management.Api.Infrastructure;
 using Wpm.Management.Domain;
+using Wpm.Management.Domain.Events;
 
 namespace Wpm.Management.Api.Application;
 
-public class SetWeightCommandHandler(ManagementDbContext dbContext,
-                                     IBreedService breedService) : ICommandHandler<SetWeightCommand>
+public class SetWeightCommandHandler : ICommandHandler<SetWeightCommand>
 {
+    private readonly ManagementDbContext dbContext;
+    private readonly IBreedService breedService;
+
+    public SetWeightCommandHandler(ManagementDbContext dbContext,
+                                   IBreedService breedService)
+    {
+        this.dbContext = dbContext;
+        this.breedService = breedService;
+
+        DomainEvents.PetWeightUpdated.Subscribe((domainEvent) =>
+        {
+            //Send a message.
+        });
+    }
     public async Task Handle(SetWeightCommand command)
     {
         var pet = await dbContext.Pets.FindAsync(command.Id);
